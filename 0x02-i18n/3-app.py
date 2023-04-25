622d66847d684d2b7c51c6c7ea30fc5e9332e04c
Malek <johnmalek54@gmail.com>
2023-04-25 15:55:34 +0000

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+A python flask app
+"""
+from flask import Flask, render_template, request
+from babel import Babel
+
+
+class Config(object):
+    """
+    Configure babel for app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match with supported languages
+    """
+    return request.accept_language.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home() -> str:
+    """
+    handle / route
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port="8000", host="0.0.0.0", debug=True)
