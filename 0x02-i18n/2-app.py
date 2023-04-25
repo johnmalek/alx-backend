809cc825c84591dfccc494e1b46d0fb9ee7e9707
Malek <johnmalek54@gmail.com>
2023-04-25 19:13:12 +0000

    Task 2 correction

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,7 +25,7 @@ def get_locale():
     """
     function to determine the best match for supported languages
     """
-    return request.accept_language.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/")
